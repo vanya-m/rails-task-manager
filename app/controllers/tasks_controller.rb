f44b9fc1ed7053f133508a5b23fe4c5bcc92058b
Vanya Kojouharova <vanya.kojouharova@gmail.com>
2022-04-09 15:57:03 +0200

    Edit a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,16 +12,24 @@ class TasksController < ApplicationController
   end
 
   def create
-    # raise
-    # @restaurant = Restaurant.new(params[:restaurant])
     @task = Task.new(task_params)
     @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
